9bf8b2ecc652c646b621f8ff2c7c1e6aab9fc63b
guizouar <guizouar@OS795.local>
2018-05-28 05:17:28 +0200

    Correction d'une erreure dans le fichier de test switch

--- Tests/switch.c
+++ Tests/switch.c
@@ -7,8 +7,9 @@ int main() {
       case 0: printd(0); break;
       case 1: printd(1); break;
       case 2: printd(2); break;
-      case 3: printd(3); 
+      case 3: printd(3);
       case 4: printd(4);
       default: printd(-1);
    }
+   return 0;    // À mentionner dans la soutenance
 }
